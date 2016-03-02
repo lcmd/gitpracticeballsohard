1be64d1e81f6f439779b8738e4b6c99ea1da4ed6
Your Name <your@example.com>
2016-03-02 10:41:41 -0500

    setting up repo

---
+++ ball so hard/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ball_so_hard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1 + 2 * 3;
+            Console.WriteLine(x++);
+            Console.WriteLine(x);
+        }
+    }
+}
